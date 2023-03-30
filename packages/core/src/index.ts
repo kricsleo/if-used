5c5fe2fdf3031a1d273d7eb21f80fcfcaacdf467
许圣峰 <kricsleo@163.com>
2023-03-30 15:02:45 +0800

    feat(WIP): collect APIs from file

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,19 +1,33 @@
-import acorn, { Options as AcornOptions } from 'acorn';
+import acorn, { Options as AcornOptions,  } from 'acorn';
 import walk from 'acorn-walk'
 import fs from 'fs/promises'
 import glob from 'fast-glob'
+import caniuse from 'caniuse-api'
+import memoize from 'lodash.memoize'
+import type { Node } from 'estree'
+
+type Browserslist = string | string[]
+type FileOptions = Pick<AcornOptions, 'ecmaVersion'> & {
+  browserslist: Browserslist
+}
 
 // async function checkFile(path: string) {
 
 // }
 
-async function checkFile(path: string | string[], options: Pick<AcornOptions, 'ecmaVersion'>) {
+async function checkFile(path: string | string[], options: FileOptions) {
   const absPaths = await glob(path, { absolute: true })
-  absPaths.forEach(absPath => async () => {
+  const fileIncompatibleNodes = await Promise.all(absPaths.map(absPath => async () => {
     const str = await fs.readFile(absPath, { encoding: 'utf-8'})
     const ast = str2AST(str, options)
-    const api = collectAPI(ast)
-  })
+    const nodes = collectAPINodes(ast)
+    const incompatibleNodes = nodes.filter(node => !checkAPI(
+      formatAPINode(node as unknown as Node), 
+      options.browserslist
+    ))
+    return incompatibleNodes
+  }))
+  return fileIncompatibleNodes
 }
 
 // async function readFiles(...path: string[]) {
@@ -26,19 +40,28 @@ function str2AST(str: string, options: AcornOptions) {
   return result
 }
 
-async function checkAPI(api: string) {
-  
+function formatAPINode(node: Node): string {
+  switch (node.type) {
+    case 'Identifier': return node.name
+    case 'MemberExpression': return formatAPINode(node.object) + '.' + formatAPINode(node.property)
+    default: return ''
+  }
 }
 
-function collectAPI(ast: acorn.Node) {
-  const api: acorn.Node[] = []
+const checkAPI = memoize(
+  (api: string, browserslist: Browserslist) => caniuse.isSupported(api, browserslist),
+  (api, browserslist) => api + browserslist
+)
+
+function collectAPINodes(ast: acorn.Node) {
+  const nodes: acorn.Node[] = []
   walk.simple(ast, {
     MemberExpression(node) {
-      api.push(node)
+      nodes.push(node)
     },
     Identifier(node) {
-      api.push(node)
+      nodes.push(node)
     }
   })
-  return api
+  return nodes
 }
