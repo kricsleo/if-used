69d7ea14c43361b29507a7262cdecbd509666168
许圣峰 <kricsleo@163.com>
2023-03-30 14:33:05 +0800

    feat(WIP): collect APIs from file

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,44 @@
-export const one = 1
-export const two = 2
+import acorn, { Options as AcornOptions } from 'acorn';
+import walk from 'acorn-walk'
+import fs from 'fs/promises'
+import glob from 'fast-glob'
+
+// async function checkFile(path: string) {
+
+// }
+
+async function checkFile(path: string | string[], options: Pick<AcornOptions, 'ecmaVersion'>) {
+  const absPaths = await glob(path, { absolute: true })
+  absPaths.forEach(absPath => async () => {
+    const str = await fs.readFile(absPath, { encoding: 'utf-8'})
+    const ast = str2AST(str, options)
+    const api = collectAPI(ast)
+  })
+}
+
+// async function readFiles(...path: string[]) {
+//   const files = await glob(path, { absolute: true })
+//    fs.readFile()
+// }
+
+function str2AST(str: string, options: AcornOptions) {
+  const result = acorn.parse(str, options)
+  return result
+}
+
+async function checkAPI(api: string) {
+  
+}
+
+function collectAPI(ast: acorn.Node) {
+  const api: acorn.Node[] = []
+  walk.simple(ast, {
+    MemberExpression(node) {
+      api.push(node)
+    },
+    Identifier(node) {
+      api.push(node)
+    }
+  })
+  return api
+}
